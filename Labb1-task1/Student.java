4ef152e140853dd0ff9306b3eab1ad05e6e3d054
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2026-04-02 11:45:09 +0200

    implumented Student.equals()

--- Labb1-task1/Student.java
+++ Labb1-task1/Student.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+
+import javax.print.FlavorException;
 public class Student implements Comparable<Student>{
 
   private final String personNumber;
@@ -20,10 +22,21 @@ public class Student implements Comparable<Student>{
   }
 
   public boolean equals(Student that){
+    
+    if (this.firstName != that.firstName) {
+      return false;
+    }
+    if (this.lastName != that.lastName) {
+      return false;
+    }
+    if (this.personNumber != that.personNumber) {
+      return false;
+    }
+    return true;
+    
     /*
     Replace return false; with your code.
     */
-    return false;
   }
 
   public int compareTo(Student that){
@@ -51,13 +64,14 @@ public class Student implements Comparable<Student>{
     Student b = new Student("0202028888", "She", "Studentdotter");
     Student c = new Student("0302017777", "He", "Studentsson");
     Student d = new Student("0302016666", "Student", "Randomsson");
+    Student e = new Student("0302016666", "Student", "Randomsson");
 
-    System.out.println(a + " equals " + a + " is " + a.equals(a));
+    System.out.println(a + " equals " + e + " is " + a.equals(e));
     System.out.println();
 
-    System.out.println(b + " equals " + c + " is " + b.equals(c));
+    System.out.println(d + " equals " + e + " is " + d.equals(e));
     System.out.println();
-
+/* 
     Student[] four = {a, b, c, d};
     System.out.println("Array with students: ");
     GenericArraySupport.printlns(four);
@@ -74,5 +88,6 @@ public class Student implements Comparable<Student>{
     d.addCredits(3);
     System.out.println("Added credits: ");
     GenericArraySupport.printlns(four);
+    */
   }
 }
